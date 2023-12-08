b63d8670d6ca292a35050dad2c41f73373981c2f
Joseph Ligier <joseph.ligier@accenture.com>
2023-12-08 12:30:10 +0100

    Add cilium_config resource

---
+++ internal/provider/cilium_config_resource.go
@@ -0,0 +1,245 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/cilium/cilium-cli/config"
+	"github.com/cilium/cilium-cli/k8s"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ resource.Resource = &CiliumConfigResource{}
+var _ resource.ResourceWithImportState = &CiliumConfigResource{}
+
+func NewCiliumConfigResource() resource.Resource {
+	return &CiliumConfigResource{}
+}
+
+// ExampleResource defines the resource implementation.
+type CiliumConfigResource struct {
+	client *k8s.Client
+}
+
+// ExampleResourceModel describes the resource data model.
+type CiliumConfigResourceModel struct {
+	Namespace types.String `tfsdk:"namespace"`
+	Restart   types.Bool   `tfsdk:"restart"`
+	Key       types.String `tfsdk:"key"`
+	Value     types.String `tfsdk:"value"`
+	Id        types.String `tfsdk:"id"`
+}
+
+func (r *CiliumConfigResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_config"
+}
+
+func (r *CiliumConfigResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		// This description is used by the documentation generator and the language server.
+		MarkdownDescription: "Config resource",
+
+		Attributes: map[string]schema.Attribute{
+			"namespace": schema.StringAttribute{
+				MarkdownDescription: "Namespace in which to install",
+				Optional:            true,
+				Computed:            true,
+				Default:             stringdefault.StaticString("kube-system"),
+			},
+			"restart": schema.BoolAttribute{
+				MarkdownDescription: "Restart Cilium pods",
+				Optional:            true,
+				Computed:            true,
+				Default:             booldefault.StaticBool(true),
+			},
+			"key": schema.StringAttribute{
+				MarkdownDescription: "Key of the config",
+				Required:            true,
+			},
+			"value": schema.StringAttribute{
+				MarkdownDescription: "Value of the key",
+				Required:            true,
+			},
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Example identifier",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+		},
+	}
+}
+
+func (r *CiliumConfigResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*k8s.Client)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *k8s.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = client
+}
+
+func (r *CiliumConfigResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data CiliumConfigResourceModel
+	k8sClient := r.client
+	var params = config.Parameters{
+		Writer: os.Stdout,
+	}
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	namespace := data.Namespace.ValueString()
+	key := data.Key.ValueString()
+	value := data.Value.ValueString()
+	params.Namespace = namespace
+	params.Restart = data.Restart.ValueBool()
+
+	check := config.NewK8sConfig(k8sClient, params)
+	if err := check.Set(context.Background(), key, value, params); err != nil {
+		fmt.Printf("Unable to set config: %s", err)
+		return
+	}
+
+	// For the purposes of this example code, hardcoding a response value to
+	// save into the Terraform state.
+	data.Id = types.StringValue("cilium-config-" + key)
+
+	// Write logs using the tflog package
+	// Documentation: https://terraform.io/plugin/log
+	tflog.Trace(ctx, "created a resource")
+
+	// Save data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *CiliumConfigResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var data CiliumConfigResourceModel
+	k8sClient := r.client
+	var params = config.Parameters{
+		Writer: os.Stdout,
+	}
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	namespace := data.Namespace.ValueString()
+	params.Namespace = namespace
+	key := data.Key.ValueString()
+	value := data.Value.ValueString()
+
+	check := config.NewK8sConfig(k8sClient, params)
+	out, err := check.View(context.Background())
+	if err != nil {
+		fmt.Printf("Unable to view config: %s", err)
+		return
+	}
+
+	m, err := regexp.MatchString(key+".*"+value, out)
+	if err != nil {
+		fmt.Println("your regex is faulty")
+		return
+	}
+	if m {
+		fmt.Println("Ok ttttttttttttt")
+	} else {
+		resp.State.RemoveResource(ctx)
+		return
+	}
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *CiliumConfigResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var data CiliumConfigResourceModel
+	k8sClient := r.client
+	var params = config.Parameters{
+		Writer: os.Stdout,
+	}
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	namespace := data.Namespace.ValueString()
+	key := data.Key.ValueString()
+	value := data.Value.ValueString()
+	params.Namespace = namespace
+	params.Restart = data.Restart.ValueBool()
+
+	check := config.NewK8sConfig(k8sClient, params)
+	if err := check.Set(context.Background(), key, value, params); err != nil {
+		fmt.Printf("Unable to set config: %s", err)
+		return
+	}
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (r *CiliumConfigResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data CiliumConfigResourceModel
+	k8sClient := r.client
+	var params = config.Parameters{
+		Writer: os.Stdout,
+	}
+
+	// Read Terraform prior state data into the model
+	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	namespace := data.Namespace.ValueString()
+	params.Namespace = namespace
+	key := data.Key.ValueString()
+	params.Restart = data.Restart.ValueBool()
+
+	check := config.NewK8sConfig(k8sClient, params)
+	if err := check.Delete(context.Background(), key, params); err != nil {
+		fmt.Printf("Unable to delete config: %s", err)
+	}
+}
+
+func (r *CiliumConfigResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -75,6 +75,7 @@ func (p *CiliumProvider) Configure(ctx context.Context, req provider.ConfigureRe
 func (p *CiliumProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
 		NewCiliumInstallResource,
+		NewCiliumConfigResource,
 		NewCiliumClusterMeshEnableResource,
 		NewCiliumClusterMeshConnectResource,
 	}
