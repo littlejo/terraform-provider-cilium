b6298a3032e635b17b798fee4e9f32300bf1666e
Joseph Ligier <joseph.ligier@accenture.com>
2024-01-05 16:02:55 +0100

    Change directory name and filenames

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/littlejo/terraform-provider-cilium/internal/provider"
+	"github.com/littlejo/terraform-provider-cilium/cilium"
 )
 
 // Run "go generate" to format example terraform files and generate the docs for the registry/website
