dd7f31446189ed8a42eaa83e9b0d5546807bf502
Joseph Ligier <joseph.ligier@accenture.com>
2024-03-26 09:37:39 +0100

    test: check cluster_mesh resource

---
+++ cilium/cluster_mesh_enable_test.go
@@ -0,0 +1,51 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccCiliumClusterMeshEnableResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccCiliumClusterMeshEnableResourceConfig("NodePort"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("cilium_clustermesh.test", "namespace", "kube-system"),
+					resource.TestCheckResourceAttr("cilium_clustermesh.test", "enable_external_workloads", "false"),
+					resource.TestCheckResourceAttr("cilium_clustermesh.test", "enable_kv_store_mesh", "false"),
+					resource.TestCheckResourceAttr("cilium_clustermesh.test", "service_type", "NodePort"),
+					resource.TestCheckResourceAttr("cilium_clustermesh.test", "id", "ciliumclustermeshenable"),
+				),
+			},
+			// ImportState testing
+			//{
+			//	ResourceName:            "cilium_clustermesh.test",
+			//	ImportState:             true,
+			//	ImportStateVerify:       true,
+			//	ImportStateVerifyIgnore: []string{"enable_external_workloads", "enable_kv_store_mesh", "service_type"},
+			//},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccCiliumClusterMeshEnableResourceConfig(service_type string) string {
+	return fmt.Sprintf(`
+resource "cilium" "test" {
+  version = "1.15.2"
+}
+resource "cilium_clustermesh" "test" {
+  service_type = %[1]q
+  depends_on   = [ cilium.test ]
+}
+`, service_type)
+}
