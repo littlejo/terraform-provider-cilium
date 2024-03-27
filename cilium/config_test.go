8dd8f6bf06c5a821380036248a3b1b11aeeeb16b
Joseph Ligier <joseph.ligier@accenture.com>
2024-03-27 09:01:50 +0100

    test: check config

---
+++ cilium/config_test.go
@@ -0,0 +1,52 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccCiliumConfigResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccCiliumConfigResourceConfig("debug", "true"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("cilium_config.test", "key", "debug"),
+					resource.TestCheckResourceAttr("cilium_config.test", "value", "true"),
+					resource.TestCheckResourceAttr("cilium_config.test", "namespace", "kube-system"),
+					resource.TestCheckResourceAttr("cilium_config.test", "restart", "true"),
+					resource.TestCheckResourceAttr("cilium_config.test", "id", "cilium-config-debug"),
+				),
+			},
+			// ImportState testing
+			//{
+			//	ResourceName:            "cilium_clustermesh.test",
+			//	ImportState:             true,
+			//	ImportStateVerify:       true,
+			//	ImportStateVerifyIgnore: []string{"enable_external_workloads", "enable_kv_store_mesh", "service_type"},
+			//},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccCiliumConfigResourceConfig(key string, value string) string {
+	return fmt.Sprintf(`
+resource "cilium" "test" {
+  version = "1.15.2"
+}
+resource "cilium_config" "test" {
+  key        = %[1]q
+  value      = %[2]q
+  depends_on = [ cilium.test ]
+}
+`, key, value)
+}
