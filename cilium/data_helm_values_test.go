7d367ddddcfe72dc8510a7f6303729e42710ebc4
Joseph Ligier <joseph.ligier@accenture.com>
2024-03-30 00:27:01 +0100

    Fix data_helm_values_test

---
+++ cilium/data_helm_values_test.go
@@ -0,0 +1,39 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccCiliumHelmValuesDataSource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccCiliumHelmValuesDataSourceConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					//resource.TestCheckResourceAttr("cilium.test", "version", "1.14.4"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccCiliumHelmValuesDataSourceConfig(version string) string {
+	return fmt.Sprintf(`
+resource "cilium" "test" {
+  version = "1.15.2"
+}
+
+data "cilium_helm_values" "test" {
+}
+`
+}
