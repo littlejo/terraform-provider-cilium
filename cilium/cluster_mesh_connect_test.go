5c5eeb977a4ac9ac884469f140b82ec6fa617409
Joseph Ligier <joseph.ligier@accenture.com>
2024-03-30 22:04:47 +0100

    Test cluster_mesh_connect_test.go

--- cilium/cluster_mesh_connect_test.go
+++ cilium/cluster_mesh_connect_test.go
@@ -18,11 +18,11 @@ func TestAccCiliumClusterMeshConnectResource(t *testing.T) {
 			// Create and Read testing
 			{
 				Config: testAccCiliumClusterMeshConnectResourceConfig(),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					//resource.TestCheckResourceAttr("cilium_clustermesh.test", "enable_external_workloads", "false"),
-					//resource.TestCheckResourceAttr("cilium_clustermesh.test", "enable_kv_store_mesh", "false"),
-					//resource.TestCheckResourceAttr("cilium_clustermesh.test", "service_type", "NodePort"),
-					//resource.TestCheckResourceAttr("cilium_clustermesh.test", "id", "ciliumclustermeshenable"),
+				Check:  resource.ComposeAggregateTestCheckFunc(
+				//resource.TestCheckResourceAttr("cilium_clustermesh.test", "enable_external_workloads", "false"),
+				//resource.TestCheckResourceAttr("cilium_clustermesh.test", "enable_kv_store_mesh", "false"),
+				//resource.TestCheckResourceAttr("cilium_clustermesh.test", "service_type", "NodePort"),
+				//resource.TestCheckResourceAttr("cilium_clustermesh.test", "id", "ciliumclustermeshenable"),
 				),
 			},
 			// ImportState testing
