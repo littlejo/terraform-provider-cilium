eb7f0f1b9fe8a70ef4dd8563708b75cb3badaa54
Joseph Ligier <joseph.ligier@accenture.com>
2024-04-03 17:49:57 +0200

    Test: cilium install on another namespace

---
+++ .github/tf/cilium/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    cilium = {
+      source  = "terraform.local/local/cilium"
+      version = "0.0.1"
+    }
+  }
+  required_version = ">= 1.3"
+}
+
+provider "cilium" {
+  namespace = "cilium"
+}
+
+provider "cilium" {
+  alias = "preflight"
+  namespace = "cilium"
+  helm_release = "cilium-preflight"
+}
+
+resource "cilium" "this" {
+  version  = "1.15.2"
+}
+
+resource "cilium" "preflight" {
+  version  = "1.15.3"
+  set = [
+    "preflight.enabled=true",
+    "agent=false",
+    "operator.enabled=false",
+  ]
+  provider = cilium.preflight
+}
