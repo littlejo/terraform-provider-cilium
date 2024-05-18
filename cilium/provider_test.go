221ce92ce2c92d7d815805bc0368634b3d2f2f0c
Joseph Ligier <joseph.ligier@accenture.com>
2024-05-18 07:06:22 +0200

    Test

--- cilium/provider_test.go
+++ cilium/provider_test.go
@@ -23,3 +23,12 @@ func testAccPreCheck(t *testing.T) {
 	// about the appropriate environment variables being set are common to see in a pre-check
 	// function.
 }
+
+const ProviderConfig = `
+	provider "cilium" {
+	}
+	provider "cilium" {
+		alias   = "test"
+		context = "kind-test"
+	}
+`
